59765181a2c12e6c58e7034c56ca4f2e6f09bde5
Rahul-Tyagi12 <61530246+Rahul-Tyagi12@users.noreply.github.com>
2020-11-22 11:51:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,22 +1,21 @@
 from tobrot.sample_config import Config
 
 class Config(Config):
-  TG_BOT_TOKEN = "1400992374:AAHEvgKn7FpZrr6rlAJbV4CYl2aKo42LHaU"
-  APP_ID = 1878625
-  API_HASH = "3ab3e48ba126de2eb96ffff39b18fa09"
-  AUTH_CHANNEL = [-1001256038785, -1001408445549, -1001398883981, -1001275361660, 715779594, 1161316820, 1291911714]
-  INDEX_LINK = "https://tgindex.hackitabirhasan.workers.dev/3:/Trash"
-  YTDL_COMMAND = "ytdl"
-  LEECH_COMMAND = "leech"
+  TG_BOT_TOKEN = "1423901717:AAEOrexGNCwp2RMIxkXyApphq6O3_OUy1UU"
+  APP_ID = 1310683
+  API_HASH = "b735d306fc6ee8773aedf5022224bcea"
+  AUTH_CHANNEL = [-1001265545284]
+  INDEX_LINK = "https://wb.godseye.workers.dev/"
+  YTDL_COMMAND = "ytdl@wb"
+  LEECH_COMMAND = "leech@wb"
   CLEAR_THUMBNAIL = "clearthumbnail"
   SAVE_THUMBNAIL = "savethumbnail"
-  OWNER_ID = 715779594
+  OWNER_ID = 762834285 
   RCLONE_CONFIG = """type = drive
 scope = drive
 root_folder_id = 0AE7pdvzvClR_Uk9PVA
-token = {"access_token":"ya29.A0AfH6SMCaLwuWkNALk5I4IWBZP6y5Y4lZRkgepXoaf-LvSu2jmNDyLzezwJk-HlgcP1OSiRKqAa9qKsCBMv9O2IMd1ebAwhYdVoNP7jIXMRBDKzD10qfrzq7oGda_Z-vP3Qvl6PQcOYC3x5ecEkU3vMIyqEC2xgANsEnowpSENeU","token_type":"Bearer","refresh_token":"1//0gHp6knwySTm9CgYIARAAGBASNwF-L9IrRPO94mU1d9kBkUu1BbYrkA_EMhGXcCiHXGULiAZDg5BmaSxhrtALRfBmVpiotPEtK6Q","expiry":"2020-11-11T13:38:54.4701885+06:00"}
-team_drive = 0AE7pdvzvClR_Uk9PVA"""
-  DESTINATION_FOLDER = "Trash"
-  GLEECH_COMMAND = "gleech"
-  TELEGRAM_LEECH_COMMAND_G = "tleech"
-  PYTDL_COMMAND_G = "pytdl"
+token = {"access_token":"ya29.A0AfH6SMCaLwuWkNALk5I4IWBZP6y5Y4lZRkgepXoaf-LvSu2jmNDyLzezwJk-HlgcP1OSiRKqAa9qKsCBMv9O2IMd1ebAwhYdVoNP7jIXMRBDKzD10qfrzq7oGda_Z-vP3Qvl6PQcOYC3x5ecEkU3vMIyqEC2xgANsEnowpSENeU","token_type":"Bearer","refresh_token":"1//0gHp6knwySTm9CgYIARAAGBASNwF-L9IrRPO94mU1d9kBkUu1BbYrkA_EMhGXcCiHXGULiAZDg5BmaSxhrtALRfBmVpiotPEtK6Q","expiry":"2020-11-11T13:38:54.4701885+06:00"}"""
+  DESTINATION_FOLDER = "Leecher"
+  GLEECH_COMMAND = "gleech@wb"
+  TELEGRAM_LEECH_COMMAND_G = "tleech@wb"
+  PYTDL_COMMAND_G = "pytdl@wb"
